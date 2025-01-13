3c4d68f482bb4d4e78ba888ff80cf09ee1fae9e9
Js-cook <jscook1001@gmail.com>
2025-01-13 18:20:23 -0500

    testing api callability and functionality

--- api/index.py
+++ api/index.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    message = {"status": "OK"}
+    return json.dumps(message)
 
 @app.route('/about')
 def about():
